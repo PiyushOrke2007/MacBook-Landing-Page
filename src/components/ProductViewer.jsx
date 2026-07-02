23c077dc3d9af368b80010db63e5fc6cb56de660
Piyush Orke <piyushorke059@gmail.com>
2026-07-03 01:03:05 +0530

    Added Lights to MacBook Model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import MacBookModel14 from "./models/Macbook-14";
-
+import StudioLights from "./StudioLights";
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacBookStore();
   return (
@@ -53,7 +53,7 @@ const ProductViewer = () => {
       <Canvas
         id="canvas"
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
-        <ambientLight intensity={0.5} />
+        <StudioLights />
         <MacBookModel14 scale={0.06} position={[0, 0, 0]} />
         <OrbitControls enableZoom={false} />
       </Canvas>
